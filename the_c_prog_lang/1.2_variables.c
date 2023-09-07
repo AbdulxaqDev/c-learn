765b6c5303e40538be2d50d1d511d0ba769fea81
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-09-07 22:56:18 +0300

    variables again

--- the_c_prog_lang/1.2_variables.c
+++ the_c_prog_lang/1.2_variables.c
@@ -1,3 +1,24 @@
-#include 
+#include <stdio.h>
+
+// symbolic constatnts
+#define LOWER  0     /* lower limit of table */
+#define UPPER  300   /* upper limit */
+#define STEP   20    /* step size */
+
+int main(){
+    /*    
+    //version 1
+    float fahr;
+
+    for (fahr = 0; fahr <= 300; fahr+=20) 
+    printf("%3.0f %6.1f\n", fahr, (5.0/9.0)*(fahr - 32.0));
+    */
+
+    // version 2
+    int fahr;
+    // for loop
+    for (fahr = LOWER; fahr <= UPPER; fahr+=STEP)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+}
 
 
