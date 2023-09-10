251e4ff94386167e3538557616ce71a90b30e196
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-09-10 23:04:51 +0300

    complete: letter and word histogram

--- the_c_prog_lang/char_word_histogram.c
+++ the_c_prog_lang/char_word_histogram.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     char let;
-    int charRepetition;
+    int letterRepetition;
 } letter;
 
 typedef struct {
@@ -10,26 +11,43 @@ typedef struct {
     int wordRepetition;
 } word;
 
-word* coutnWords();
+typedef struct {
+    letter* letters;
+    word* words;
+} pCharWord;
+
+
+
+pCharWord  countCharWord();
 void wordsLenHistogram(word*);
-
-
-void charListInsert(letter[], letter);
+void letterHistogram(letter*);
+void letterListInsert(letter[], char);
 void wordListInsert(word[], int);
 
 
-int main(){
-    word* lenOfWords = coutnWords();
+int main()
+{
+    pCharWord charWordList = countCharWord();
+    letter* lettersList = charWordList.letters;
+    word* wordsList = charWordList.words;
 
-    wordsLenHistogram(lenOfWords);
+    wordsLenHistogram(wordsList);
+    letterHistogram(lettersList);
 
     return (0);
 }
 
-void wordsLenHistogram(word* words){
-    printf("\n\nWords histogram based on their length");
+void wordsLenHistogram(word* words)
+{
+    printf("\n\n -- Words histogram based on their length --\n");
+    printf("\nFollowing graph shows repetition of words with");
+    printf("\nsame length in bar and exact number. If words\nwith same legth repeats more, the bar becames\nmore long, at the end of bar the exact number\nof them is given:\n");
+    printf("\n         [5]■■■■■■■■■■■■■■■■■■■■■■ x22");
+    printf("\n         /             |             \\      ");
+    printf("\nword length           bar       repetition number\n");
 
-    for (int i = 0; i < 10; ++i) 
+
+    for (int i = 0; i < 300; ++i) 
     {
         if(words[i].wordLen > 0)
         {
@@ -44,10 +62,62 @@ void wordsLenHistogram(word* words){
     printf("\n");
 }
 
-word* coutnWords(){
-    word words[300] = {0, 0};
-    word* pWords = words;
+void letterHistogram(letter* letters)
+{
+    printf("\n\n -- Ltters histogram based on their repetition --\n");
+    printf("\nFollowing graph shows repetition of a letter");
+    printf("\nin bar and its repetition in number. If letter");
+    printf("\nrepeats more, the bar becames more long, at the");
+    printf("\nend of bar the exact repetition number is given:\n");
+    printf("\n         ['c']■■■■■■■■■■■■■■■■■■■■■■ x22");
+    printf("\n          /             |             \\      ");
+    printf("\n     letter         histogram    repetition number\n");
+
+
+
+
+    for (int i = 0; i < 256; ++i) 
+    {
+        if(letters[i].let > 0)
+        {
+
+            if(letters[i].let == '\n')
+                 printf("\n['\\n']");
+            else
+                printf("\n['%c']", letters[i].let);
+            for (int j = 0; j < letters[i].letterRepetition; ++j) 
+            {
+                printf("■");
+            }
+            printf(" x%d", letters[i].letterRepetition);
+        }
+    }
+    printf("\n");
+
+}
+
+pCharWord countCharWord()
+{
+    word* words = malloc(300 * sizeof(word));
+    letter* letters = malloc(300 * sizeof(letter));
+
     int c, wordLen, wordIndex;
+    pCharWord returnType;
+    returnType.words = words;
+    returnType.letters = letters;
+
+    for (int i = 0; i < 300; i++) 
+    {
+        words[i].wordLen = 0;
+        words[i].wordRepetition = 0;
+    }
+
+
+    for (int i = 0; i < 256; i++) 
+    {
+        letters[i].let = 0;
+        letters[i].letterRepetition = 0;
+    }
 
     wordLen = 0;
     wordIndex = 0;
@@ -56,29 +126,35 @@ word* coutnWords(){
     {   
         if(c == ' ' || c == '\n' || c == '\t')
         {   
-            wordListInsert(words, wordLen);
+            if(wordLen > 0)
+                wordListInsert(words, wordLen);
             wordLen = 0;
         }   
         else
         {
             ++wordLen;
         }
+        
+        letterListInsert(letters, c);
+
     }
     wordListInsert(words, wordLen);
 
-    return pWords;
+
+    return returnType;
 }
 
-void wordListInsert(word words[], int wordLen){
+void wordListInsert(word words[], int wordLen)
+{
     int isInList = 0;
-    for (int i = 0; i < 300; ++i) {
+    for (int i = 0; i < 300; ++i)
+    {
         if(words[i].wordLen == wordLen)
         {
             ++words[i].wordRepetition;
             isInList = 1;
         }
     }
-
     if(isInList == 0)
     {
         for (int i = 0; i < 300; ++i)
@@ -93,4 +169,39 @@ void wordListInsert(word words[], int wordLen){
             }
         }
     }
+
 }
+
+
+void letterListInsert(letter letters[], char let)
+{
+    int isInList = 0;
+    for (int i = 0; i < 256; ++i)
+    {
+        if(letters[i].let == let)
+        {
+            ++letters[i].letterRepetition;
+            isInList = 1;
+        }
+    }
+
+    if(isInList == 0)
+    {
+        for (int i = 0; i < 300; ++i)
+        {
+            if(letters[i].let == 0)
+            {
+                letter newLetter;
+                newLetter.let = let;
+                newLetter.letterRepetition = 1;
+                letters[i] = newLetter;
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+
