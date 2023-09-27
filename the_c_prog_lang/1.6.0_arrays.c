db4aa56f44fc204550061179e5c1729d8069032a
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-09-27 16:10:50 +0300

    functions

--- the_c_prog_lang/1.6.0_arrays.c
+++ the_c_prog_lang/1.6.0_arrays.c
@@ -2,38 +2,32 @@
 
 /* count digits, white space, others */
 int main(){
-    int c, i, nwhite, nother;
-    int ndigit[10];
-    
-    printf("use Ctrl+D to exit program on Linux, on Windows Ctrl+Z\n");
+  int c, i, nwhite, nother;
+  int ndigit[10];
 
-    for (i = 0; i < 10; ++i)
-    {
-        ndigit[i] = 0;
-    }
+  printf("use Ctrl+D to exit program on Linux, on Windows Ctrl+Z\n");
+  nwhite = nother = 0;
+  for (i = 0; i < 10; ++i)
+  {
+    ndigit[i] = 0;
+  }
 
-    while((c = getchar()) != EOF)
-    {
-        if(c >= '0' && c <= '9')
-        {
-            ++ndigit[c - '0'];
-        } else if(c == ' ' || c == '\n' || c == '\t')
-        {
-            ++nwhite;
-        } else
-        {
-            ++nother;
-        }
-    }
-    printf("\ndigits =");
-    for (i = 0; i < 10; ++i) 
-    {
-        printf(" %d", ndigit[i]);
-    }
+  while((c = getchar()) != EOF)
+    if('0' <= c && c <= '9')
+      ++ndigit[c - '0'];
+    else if(c == ' ' || c == '\n' || c == '\t')
+      ++nwhite;
+    else
+      ++nother;
+  
+  printf("\ndigits =");
 
-    printf(", white space = %d, other = %d\n", nwhite, nother);
+  for (i = 0; i < 10; ++i) 
+    printf(" %d", ndigit[i]);
 
+  printf(", white space = %d, other = %d\n", nwhite, nother);
 
+  printf("%d\n", '8');
 
-    return (0);
+  return (0);
 }
--- the_c_prog_lang/projects/x_o.c
+++ the_c_prog_lang/projects/x_o.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <termios.h>
 
+struct termios info;
+
 int main( int argc, char *argv[])
 {
 
-     int c;
+    tcgetattr(0, &info);          /* get current terminal attirbutes; 0 is the file descriptor for stdin */
+    info.c_lflag &= ~ICANON;      /* disable canonical mode */
+    info.c_cc[VMIN] = 1;          /* wait until at least one keystroke available */
+    info.c_cc[VTIME] = 0;         /* no timeout */
+    tcsetattr(0, TCSANOW, &info); /* set immediately */
 
-   while((c = getchar()) != EOF )
-      if(c == 27)   break;
-/* 27 is the ASCII code for Esc */
 
-    return 0;
+    int c;
+    while((c = getchar()) != 27 /* ascii ESC */) {
+        if (c < 0) {
+            if (ferror(stdin)) { /* there was an error... */ }
+            clearerr(stdin);
+            /* do other stuff */
+        } else {
+            /* some key OTHER than ESC was hit, do something about it? */
+            printf("you pressed - %c\n", c);
+		}
+	}
+
+
+  /**/      
+
+	tcgetattr(0, &info);
+	info.c_lflag |= ICANON;
+	tcsetattr(0, TCSANOW, &info);
 
 }
